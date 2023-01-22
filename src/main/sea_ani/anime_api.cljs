085062c45579b88c0159bd90aa6b3c6d7f9b05c1
sabingurung <sabingurung68@gmail.com>
2023-01-22 16:58:58 +0100

    modal implemented

--- src/main/sea_ani/anime_api.cljs
+++ src/main/sea_ani/anime_api.cljs
@@ -2,11 +2,26 @@
   (:require
    [sea-ani.http :refer [http]]))
 
+(defn- api-season-anime [year season page]
+  (str "https://api.jikan.moe/v4/seasons/" year "/" season "?page=" page))
+(defn- api-anime-full [mal_id]
+  (str "https://api.jikan.moe/v4/anime/" mal_id "/full"))
+
 (defn anime-by-season [year season page cb]
   (let [map-anime (comp
                     (map #(select-keys % [:title_english :score :images :mal_id]))
                     (map #(update % :images (comp :image_url :jpg)))
                     (filter :title_english))]
-    (http :get (str "https://api.jikan.moe/v4/seasons/" year "/" season "?page=" page)
+    (http :get (api-season-anime year season page)
           :cb #(cb (update % :data (fn [c] (sequence map-anime c)))))))
+
+(defn anime-full [mal_id cb]
+  (http :get (api-anime-full mal_id)
+        :cb #(-> (:data %)
+                 (select-keys [:title_english :popularity :trailer
+                               :synopsis :images :trailer])
+                 (update :trailer :embed_url)
+                 (update :images (comp :large_image_url :jpg))
+                 cb)))
+
 
--- src/main/sea_ani/app.cljs
+++ src/main/sea_ani/app.cljs
@@ -1,10 +1,9 @@
 (ns sea-ani.app
   (:require
    [reagent.core :as r]
-   [sea-ani.anime-api :refer [anime-by-season]]))
+   [sea-ani.anime-api :refer [anime-by-season anime-full]]))
 
 ; ========================= model app states ======================================
-
 (defonce app-state
   (r/atom {:search-params {}
            :search-results nil}))
@@ -27,7 +26,32 @@
              (assoc-in [:search-results :pagination] (:pagination %))
              (->> (reset! app-state)))))))
 
-; ========================= views ui ======================================
+; ========================= modal helper views ======================================
+(defonce model-state (r/atom {:open? false :component nil}))
+
+(defn launch-modal! [component]
+  (-> @model-state
+      (assoc :open? true)
+      (assoc :component component)
+      (->> (reset! model-state))))
+
+(defn close-modal! []
+  (-> @model-state
+      (assoc :open? false)
+      (assoc :component nil)
+      (->> (reset! model-state))))
+
+(defn modal []
+  [:dialog#modal {:open (:open? @model-state)}
+   [:article
+    [:a.close {:aria-lable "Close"
+               :data-target "modal"
+               :on-click close-modal!}]
+    (when-let [component (:component @model-state)]
+      component)]])
+
+; ========================= views ======================================
+
 (defn current-year [] (.. (js/Date.) getFullYear))
 (def years (range (current-year) 1963 -1))
 (def seasons ["summer" "fall" "winter" "spring"])
@@ -58,6 +82,20 @@
          (for [i seasons] ^{:key i}[:option {:value i} i])]]]
       [:button {:on-click #(load-anime! @year @season 1)} "Search"]]]))
 
+(defn anime-card [anime_id]
+  #_{:clj-kondo/ignore [:unresolved-symbol]}
+  (r/with-let [data (r/atom {})
+               _ (anime-full anime_id #(reset! data %))]
+    [:<>
+     [:h3 (:title_english @data)]
+     [:figure
+      [:img {:src (:images @data) }]
+      [:figcaption (:synopsis @data)]]
+     [:kbd "Popularity : " (:popularity @data)]
+     [:hr]
+     (when-let [trailer (:trailer @data)]
+       [:iframe {:src trailer :width "100%" :height "385px" }])]))
+
 (defn anime-list [{:keys [pagination data]}]
   [:div.container-fluid
    [:div.anime-list
@@ -65,10 +103,11 @@
       ^{:key (:mal_id anime)}
       [:article.anime
        [:figure
+        (println anime)
         [:img {:src (:images anime)}]
-        (when-let [score (:score anime)]
-          [:figcaption [:kbd (:score anime)]])
-        (:title_english anime) ]])]
+        [:figcaption (:title_english anime)] [:hr]
+        [:span {:role "button"
+                :on-click #(launch-modal! [anime-card (:mal_id anime)]) } "More"]]])]
    (when (:has_next_page pagination)
      [:button {:on-click #(load-more!)} "Load More"])])
 
@@ -76,14 +115,20 @@
   [:div
    [header]
    [search]
-   [anime-list (:search-results @app-state)]])
+   [anime-list (:search-results @app-state)]
+   [modal]
+   ])
 
 (comment
+  (-> @model-state)
+  (launch-modal! [ (fn [] [:h1 "heelo there baby"]) ])
+  (close-modal!)
   (js/alert "hello")
   years
   seasons
   (anime-by-season 2021 "spring" 1 #(def a %))
   (load-anime! 2021 "fall" 1)
+  @model-state
   (-> a
       (update :data (partial take 10))
       )
